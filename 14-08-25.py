53451d0f103ab8a9b84ef535292bd4263cfbf404
Priya-236 <lakshmipriyadurgachinnam@gmail.com>
2025-08-15 11:11:10 +0530

    Create 14-08-25.py

---
+++ 14-08-25.py
@@ -0,0 +1,28 @@
+#sum of first n natural numbers
+num=5
+sum=0
+for i in range(num+1):
+       sum+=i
+print(sum)
+#sum of n  number in a given range
+num1,num2 =3,6
+sum=0
+for i in range(num1,num2+1);
+        sum+=i
+print(sum)
+#sum of n natural numbers
+number,sum=6,0
+for i in range(number+1):
+      sum+=i
+print(sum)
+#prime number
+num=15
+flag=0
+for i in range (2,num):
+  if num%i==0:
+    flag=1
+    break
+    if flag ==1:
+      print("not prime")
+    else:
+      print("prime")
